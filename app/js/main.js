9e998d12fa90e39591f7019feb9986a216c087c4
Benjie Songsong <benjiesongsong@gmail.com>
2016-04-12 15:59:23 -0400

    Working on new project because last one "deleted" - currently figuring out how to work setInterval with clock.# Please enter the commit message for your changes. Lines starting

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
 console.log('Hello New Project!');
+
+var currentTime = new Date ();
+  var hr = currentTime.getHours ();
+  var min = currentTime.getMinutes ();
+  var sec = currentTime.getSeconds ();
+
+var clock = hr + ":" + min + ":" + sec;
+// var currentClock = setInterval(currentTime, 1000);
+
+
+var clockAnswer = document.querySelector("#clock");
+
+clockAnswer.innerHTML = currentClock;
