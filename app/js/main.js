5a55873fa3b9cb26a503b50bf0852e72548fda6c
Benjie Songsong <benjiesongsong@gmail.com>
2016-04-12 18:04:55 -0400

    Was able to finish the entire clock and change the background as well.

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,32 @@
 console.log('Hello New Project!');
 
 // Make a variable that will display current date
-var currentClock = new Date ()
-  var hr = currentClock.getHours ()
-  var min = currentClock.getMinutes ()
-  var sec = currentClock.getSeconds ()
-    var timer = hr + ":" + min + ":" + sec;
+var showClock = document.querySelector("#clock");
+var changeBg = document.querySelector("body")
 
-var addSecond = (currentClock, 1000);
-// Display Clock on HTML
-var clockAnswer = document.querySelector("#clock");
-clockAnswer.innerHTML = addSecond;
+function currentClock () {
+  var currentTime = new Date ();
+  // Getting hours in current time
+  var hr = currentTime.getHours ();
+    // Subtracts -12 from hours that are over 12 hrs to prevent military time.
+    var newhr = (hr > 12 || hr < 21) ? "0" + (hr - 12) : hr;
+  // Getting minutes in current time
+  var min = currentTime.getMinutes ();
+    // Adds a 0 string on the minute when it's less than 10 seconds.
+    var newmin = (min < 10) ? "0" + min : min;
+  /// Getting seconds in current time.
+  var sec = currentTime.getSeconds ();
+    // Adds a 0 string on the minute when it's less than 10 seconds.
+    var newsec = (sec < 10) ? "0" + sec : sec;
+  var clockDisplay = (newhr + ":" + newmin + ":" + newsec);
+  var hexaColor = ("#" + newhr + "" + newmin + "" + newsec);
+  showClock.innerHTML = clockDisplay;
+  showClock.innerHTML = hexaColor;
+  changeBg.style.background = hexaColor;
+}
+
+// function colorChange () {
+//   background.style.background =
+// }
+
+var finalClock = setInterval(currentClock, 1000);
