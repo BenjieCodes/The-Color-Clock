011108fa43aaf492522f20e7b36c222b0400fb51
Benjie Songsong <benjiesongsong@gmail.com>
2016-04-12 15:13:05 -0400

    Figured out how to display clock, now to figure out how to setInterval to add seconds.

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,13 @@
 console.log('Hello New Project!');
+
+// Make a variable that will display current date
+var currentClock = new Date ()
+  var hr = currentClock.getHours ()
+  var min = currentClock.getMinutes ()
+  var sec = currentClock.getSeconds ()
+    var timer = hr + ":" + min + ":" + sec;
+
+var addSecond = (currentClock, 1000);
+// Display Clock on HTML
+var clockAnswer = document.querySelector("#clock");
+clockAnswer.innerHTML = addSecond;
